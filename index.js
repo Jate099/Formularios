db7a854417b741bca4ea0a55464fd1a85791ccef
Javier Andres Trochez Enciso <1143878729@academico.icesi.edu.co>
2019-04-12 08:24:26 -0500

    se crea un formulario

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ var app = express();
 app.use(express.static('public'));
 
 app.engine('handlebars', render());
-app.set('vew engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
+app.get('/', function(request, response){
+    var context = {
+        titulo: 'pagina principal',
+    };
 
+    response.render('home', context);
+});
+
+app.post('/login', function(request, response){
+    console.log('salu2');
+    response.send('salu2');
+});
 
 app.listen(3000);
